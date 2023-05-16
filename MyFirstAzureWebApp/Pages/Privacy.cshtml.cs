d20629457b299a35dc67e3ca60e2a2f16fbc8d21
Hina-Shahzadi-08 <114351340+Hina-Shahzadi-08@users.noreply.github.com>
2023-05-16 09:33:39 +0500

    add files

--- MyFirstAzureWebApp/Pages/Privacy.cshtml.cs
+++ MyFirstAzureWebApp/Pages/Privacy.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Assignment_Demo.Pages;
+namespace MyFirstAzureWebApp.Pages;
 
 public class PrivacyModel : PageModel
 {
